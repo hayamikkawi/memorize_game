c953909c7b741057604e4ad18b7e14daec284352
Haya Mikkawi <43251570+hayamikkawi@users.noreply.github.com>
2023-11-19 22:46:47 +0200

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Haya Mikkawi on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
